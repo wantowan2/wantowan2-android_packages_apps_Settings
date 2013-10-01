324c5350801dff02d6bcc78a327ffd3ef826592d
scott <scott@ubuntu.com>
2013-10-01 02:26:59 -0400

    Overhaul to about beanstalk section.  Sources pulled from rootbox devs. Signed-off-by: scott <scott@ubuntu.com>
    Change-Id: Ibd6c92a047e05dcba068363bb57215a89b8a77f9
    Signed-off-by: Scott <scott@ubuntu>
    
    Conflicts:
    	res/values/attrs.xml

--- src/com/android/settings/beanstalk/About.java
+++ src/com/android/settings/beanstalk/About.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2012 Android Open Source Project
+ * Copyright (C) 2012 RootBox Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.settings.beanstalk;
 
 import android.content.Intent;
@@ -18,15 +35,13 @@ public class About extends SettingsPreferenceFragment {
 
     public static final String TAG = "About";
 
-    Preference mSourceUrl;
-    Preference mFacebookUrl;
+    Preference mSiteUrl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.about_rom);
-        mSourceUrl = findPreference("beanstalk_source");
-        mFacebookUrl = findPreference("beanstalk_facebook");
+        mSiteUrl = findPreference("beanstalk_site");
 
         PreferenceGroup devsGroup = (PreferenceGroup) findPreference("devs");
         ArrayList<Preference> devs = new ArrayList<Preference>();
@@ -46,10 +61,8 @@ public class About extends SettingsPreferenceFragment {
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-         if (preference == mSourceUrl) {
-            launchUrl("http://github.com/scotthartbti");
-        } else if (preference == mFacebookUrl) {
-            launchUrl("http://www.facebook.com/pages/Beanstalk/421499131276932");
+        if (preference == mSiteUrl) {
+            launchUrl("http://www.scotthartbti.com");
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -59,5 +72,12 @@ public class About extends SettingsPreferenceFragment {
         Uri uriUrl = Uri.parse(url);
         Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
         getActivity().startActivity(donate);
+        Intent github = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(github);
+        Intent google = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(google);
+        Intent facebook = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(facebook);
     }
 }
+
