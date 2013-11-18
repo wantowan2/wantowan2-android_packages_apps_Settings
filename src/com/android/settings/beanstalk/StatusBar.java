6357461aba904a285bb809daf4fc2ee39d8f1ede
Scott <scott@ubuntu>
2013-11-18 02:49:35 -0500

    fix derp
    Change-Id: If3e06985af8c48f2641c94b225e2c1bd3da22110
    Signed-off-by: Scott <scott@ubuntu>

--- src/com/android/settings/beanstalk/StatusBar.java
+++ src/com/android/settings/beanstalk/StatusBar.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.slim;
+package com.android.settings.beanstalk;
 
 import android.content.ContentResolver;
 import android.database.ContentObserver;
