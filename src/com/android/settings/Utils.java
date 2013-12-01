TeamHellfire/android_packages_apps_Settings

Add back hardware key settings.
Change-Id: I925fd9d9c02b86d8e93c8b9e25870433f7c20bde

src/com/android/settings/Utils.java
@@ -691,4 +691,9 @@ public class Utils {
     public static boolean isTablet(Context context) {
         return getScreenType(context) == DEVICE_TABLET;
     }
+
+    /* returns whether the device has volume rocker or not. */
+    public static boolean hasVolumeRocker(Context con) {
+        return con.getResources().getBoolean(R.bool.has_volume_rocker);
+    }
 }

wantowan2/wantowan2-android_packages_apps_Settings

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	res/values/cm_strings.xml
	src/com/android/settings/Utils.java

src/com/android/settings/Utils.java
@@ -691,4 +691,9 @@ public class Utils {
     public static boolean isTablet(Context context) {
         return getScreenType(context) == DEVICE_TABLET;
     }
+
+    /* returns whether the device has volume rocker or not. */
+    public static boolean hasVolumeRocker(Context context) {
+        return context.getResources().getBoolean(R.bool.has_volume_rocker);
+    }
 }