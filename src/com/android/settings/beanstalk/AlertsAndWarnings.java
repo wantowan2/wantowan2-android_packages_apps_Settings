eda90883a058bccab61a478eec4df6a01609a0b4
scott <scott@ubuntu>
2013-12-04 00:04:23 -0500

    change settings layout/naming a bit
    Change-Id: Ie937f743b220193bf7fadf7219489951696a0aa8
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/beanstalk/AlertsAndWarnings.java
+++ src/com/android/settings/beanstalk/AlertsAndWarnings.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2013 SlimRoms
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.settings.beanstalk;
 
 import android.app.AlertDialog;
@@ -38,7 +22,7 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.ArrayList;
 
-public class LightsAndWarnings extends SettingsPreferenceFragment implements
+public class AlertsAndWarnings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String KEY_LOW_BATTERY_WARNING_POLICY = "pref_low_battery_warning_policy";  
@@ -51,7 +35,7 @@ public class LightsAndWarnings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.lights_and_warnings);
+        addPreferencesFromResource(R.xml.alerts_and_warnings);
 
 	// Low battery warning
         mLowBatteryWarning = (ListPreference) findPreference(KEY_LOW_BATTERY_WARNING_POLICY);
