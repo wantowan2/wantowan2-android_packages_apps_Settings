SlimRoms/packages_apps_Settings

settings: qs tweeks
- fix fc problem on tablets
- fix filtering of unsupported tiles
- add music tile options (disable artwork, disable track title optional)
- add instance handling as well to the tile chooser dialog
- add cancel button to chooser dialog
- battery tile short press now battery stats - longpress batteryicon settings
- rotation tile lonpress - rotation settings

prevend fc on devices without mobile

Change-Id: If53fa598eb261b803dbacd4384220bdfe28df04a

src/com/android/settings/Settings.java
@@ -84,7 +84,9 @@ import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintServiceSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
+import com.android.settings.slim.BatteryIconStyle;
 import com.android.settings.slim.blacklist.BlacklistSettings;
+import com.android.settings.slim.DisplayRotation;
 import com.android.settings.slim.QuietHours;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
@@ -356,6 +358,8 @@ public class Settings extends PreferenceActivity
         KeyboardLayoutPickerFragment.class.getName(),
         BlacklistSettings.class.getName(),
         QuietHours.class.getName(),
+        BatteryIconStyle.class.getName(),
+        DisplayRotation.class.getName()
     };
 
     @Override
@@ -1128,4 +1132,6 @@ public class Settings extends PreferenceActivity
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
+    public static class BatteryIconStyleSettingsActivity extends Settings { /* empty */ }
+    public static class DisplayRotationSettingsActivity extends Settings { /* empty */ }
 }

src/com/android/settings/slim/NotificationDrawerQsSettings.java
@@ -29,6 +29,7 @@ import android.provider.Settings;
 import com.android.internal.util.slim.DeviceUtils;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.slim.quicksettings.QuickSettingsUtil;
 import com.android.settings.R;
 import com.android.settings.widget.SeekBarPreference;
 
@@ -100,6 +101,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
     @Override
     public void onResume() {
         super.onResume();
+        QuickSettingsUtil.updateAvailableTiles(getActivity());
     }
 
     @Override

wantowan2/wantowan2-android_packages_apps_Settings

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	res/values/bs_strings.xml
	src/com/android/settings/Settings.java

src/com/android/settings/Settings.java
@@ -84,7 +84,9 @@ import com.android.settings.inputmethod.SpellCheckersSettings;
 import com.android.settings.inputmethod.UserDictionaryList;
 import com.android.settings.location.LocationSettings;
 import com.android.settings.nfc.AndroidBeam;
+import com.android.settings.beanstalk.BatteryIconStyle;
 import com.android.settings.beanstalk.QuietHours;
+import com.android.settings.beanstalk.DisplayRotation;
 import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintServiceSettingsFragment;
@@ -364,7 +366,9 @@ public class Settings extends PreferenceActivity
         KeyboardLayoutPickerFragment.class.getName(),
         BlacklistSettings.class.getName(),
         ApnSettings.class.getName(),
-	QuietHours.class.getName()
+	QuietHours.class.getName(),
+	BatteryIconStyle.class.getName(),
+        DisplayRotation.class.getName()
     };
 
     @Override
@@ -1176,4 +1180,6 @@ public class Settings extends PreferenceActivity
     public static class AboutActivity extends Settings { /* empty */ }
     public static class ASSRamBarActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
+    public static class BatteryIconStyleSettingsActivity extends Settings { /* empty */ }
+    public static class DisplayRotationSettingsActivity extends Settings { /* empty */ }
 }

src/com/android/settings/beanstalk/NotificationDrawerQsSettings.java
@@ -29,6 +29,7 @@ import android.provider.Settings;
 import com.android.internal.util.beanstalk.DeviceUtils;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.beanstalk.quicksettings.QuickSettingsUtil;
 import com.android.settings.R;
 import com.android.settings.beanstalk.SeekBarPreference;
 
@@ -100,6 +101,7 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment
     @Override
     public void onResume() {
         super.onResume();
+        QuickSettingsUtil.updateAvailableTiles(getActivity());
     }
 
     @Override