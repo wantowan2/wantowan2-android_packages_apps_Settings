48fa6c1817d1247fd0c4ab8b9fc18ec3cd61b3b9
scott <scott@ubuntu>
2013-12-08 06:04:52 -0500

    fix quiet hours and move to beanstalk folder
    Change-Id: I78fee7316bdd5958c4b26d26d201cb2441785c60
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -84,6 +84,7 @@ import com.android.settings.inputmethod.SpellCheckersSettings;
 import com.android.settings.inputmethod.UserDictionaryList;
 import com.android.settings.location.LocationSettings;
 import com.android.settings.nfc.AndroidBeam;
+import com.android.settings.beanstalk.QuietHours;
 import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintServiceSettingsFragment;
@@ -362,7 +363,8 @@ public class Settings extends PreferenceActivity
         PaymentSettings.class.getName(),
         KeyboardLayoutPickerFragment.class.getName(),
         BlacklistSettings.class.getName(),
-        ApnSettings.class.getName()
+        ApnSettings.class.getName(),
+	QuietHours.class.getName()
     };
 
     @Override
--- src/com/android/settings/beanstalk/TimeRangePreference.java
+++ src/com/android/settings/beanstalk/TimeRangePreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.cyanogenmod;
+package com.android.settings.beanstalk;
 
 import java.util.Calendar;
 import java.util.Date;
