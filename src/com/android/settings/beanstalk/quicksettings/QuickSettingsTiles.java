SlimRoms/packages_apps_Settings

QS: make dynamic tiles configurable (2/2)
instead of using a bunch of provider settings var
like it was on 4.3 and almost all other roms are doing
use one and split like we do for the normal tiles. This
is more consistent and saves us to observe a bunch of values

As well this is integrated into the one layout screen we have. So
all QS related is easy and logical usable for the user on one
screen

PatchSet: added icon - thanks to kroz

Change-Id: I32756584018585644a71bce52e48a978993d27ce

src/com/android/settings/slim/quicksettings/QuickSettingsTiles.java
@@ -16,6 +16,8 @@
 
 package com.android.settings.slim.quicksettings;
 
+import static com.android.internal.util.slim.QSConstants.TILE_DELIMITER;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -62,8 +64,9 @@ import java.util.Comparator;
 
 public class QuickSettingsTiles extends Fragment {
 
-    private static final int MENU_RESET = Menu.FIRST;
-    private static final int MENU_HELP  = MENU_RESET + 1;
+    private static final int MENU_RESET         = Menu.FIRST;
+    private static final int MENU_DYNAMICTILES  = MENU_RESET + 1;
+    private static final int MENU_HELP          = MENU_DYNAMICTILES + 1;
 
     private static final String SEPARATOR = "OV=I=XseparatorX=I=VO";
 
@@ -75,6 +78,7 @@ public class QuickSettingsTiles extends Fragment {
     private static final int DLG_SHOW_LIST     = 5;
     private static final int DLG_HELP          = 6;
     private static final int DLG_DISABLED      = 7;
+    private static final int DLG_DYNAMICTILES  = 8;
 
     private DraggableGridView mDragView;
     private ViewGroup mContainer;
@@ -314,6 +318,9 @@ public class QuickSettingsTiles extends Fragment {
         menu.add(0, MENU_RESET, 0, R.string.reset)
                 .setIcon(R.drawable.ic_settings_backup) // use the backup icon
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0, MENU_DYNAMICTILES, 0, R.string.dynamic_tiles_title)
+                .setIcon(R.drawable.ic_settings_dynamic_tiles)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         menu.add(0, MENU_HELP, 0, R.string.help_label)
                 .setIcon(R.drawable.ic_settings_about)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
@@ -324,6 +331,9 @@ public class QuickSettingsTiles extends Fragment {
         switch (item.getItemId()) {
             case MENU_RESET:
                 showDialogInner(DLG_RESET);
+                return true;
+            case MENU_DYNAMICTILES:
+                showDialogInner(DLG_DYNAMICTILES);
                 return true;
             case MENU_HELP:
                 showDialogInner(DLG_HELP);
@@ -578,6 +588,68 @@ public class QuickSettingsTiles extends Fragment {
                                 QuickSettingsUtil.TILES.get(
                                     getOwner().mTileAdapter.getTileId(position));
                             getOwner().addTile(info.getTitleResId(), info.getIcon(), 0, true);
+                        }
+                    })
+                    .create();
+                case DLG_DYNAMICTILES:
+                    final ArrayList<String> allTiles =
+                        QuickSettingsUtil.getAllDynamicTiles(getActivity());
+                    String dynamicTiles = Settings.System.getStringForUser(
+                            getActivity().getContentResolver(),
+                            Settings.System.QUICK_SETTINGS_DYNAMIC_TILES,
+                            UserHandle.USER_CURRENT);
+                    if (dynamicTiles == null) {
+                        // default all dynamic tiles are turned on
+                        dynamicTiles = TextUtils.join(TILE_DELIMITER, allTiles);
+                    }
+
+                    final ArrayList<String> actualEntries = new ArrayList<String>();
+                    final ArrayList<Boolean> actualTilesStatus = new ArrayList<Boolean>();
+
+                    boolean detected;
+                    for (String tile : allTiles) {
+                        detected = false;
+                        for (String actualTile : dynamicTiles.split(
+                                "\\" + TILE_DELIMITER)) {
+                            if (tile.equals(actualTile)) {
+                                detected = true;
+                            }
+                        }
+                        actualTilesStatus.add(detected);
+                        actualEntries.add(
+                            QuickSettingsUtil.getDynamicTileDescription(getActivity(), tile));
+                    }
+
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.dynamic_tiles_title)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setMultiChoiceItems(
+                        actualEntries.toArray(new String[actualEntries.size()]),
+                        QuickSettingsUtil.toPrimitiveArray(actualTilesStatus),
+                        new  DialogInterface.OnMultiChoiceClickListener() {
+                        public void onClick(DialogInterface dialog, int indexSelected,
+                                boolean isChecked) {
+                            actualTilesStatus.set(indexSelected, isChecked);
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            String savedTiles = "";
+                            for (int i = 0; i < actualTilesStatus.size(); i++) {
+                                if (actualTilesStatus.get(i)) {
+                                    savedTiles += allTiles.get(i);
+                                    savedTiles += TILE_DELIMITER;
+                                }
+                            }
+                            if (!savedTiles.isEmpty()) {
+                                savedTiles = savedTiles.substring(0, savedTiles.length() - 1);
+                            }
+                            Settings.System.putString(
+                                getActivity().getContentResolver(),
+                                Settings.System.QUICK_SETTINGS_DYNAMIC_TILES,
+                                savedTiles);
                         }
                     })
                     .create();

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -18,13 +18,17 @@
 package com.android.settings.slim.quicksettings;
 
 import static com.android.internal.util.slim.QSConstants.TILES_DEFAULT;
+import static com.android.internal.util.slim.QSConstants.DYNAMIC_TILES_DEFAULT;
 import static com.android.internal.util.slim.QSConstants.TILE_AIRPLANE;
+import static com.android.internal.util.slim.QSConstants.TILE_ALARM;
 import static com.android.internal.util.slim.QSConstants.TILE_AUTOROTATE;
 import static com.android.internal.util.slim.QSConstants.TILE_BATTERY;
 import static com.android.internal.util.slim.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.slim.QSConstants.TILE_BRIGHTNESS;
+import static com.android.internal.util.slim.QSConstants.TILE_BUGREPORT;
 import static com.android.internal.util.slim.QSConstants.TILE_DELIMITER;
 import static com.android.internal.util.slim.QSConstants.TILE_EXPANDEDDESKTOP;
+import static com.android.internal.util.slim.QSConstants.TILE_IMESWITCHER;
 import static com.android.internal.util.slim.QSConstants.TILE_LOCATION;
 import static com.android.internal.util.slim.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.slim.QSConstants.TILE_LTE;
@@ -39,6 +43,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.slim.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.slim.QSConstants.TILE_SYNC;
 import static com.android.internal.util.slim.QSConstants.TILE_TORCH;
+import static com.android.internal.util.slim.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.slim.QSConstants.TILE_USER;
 import static com.android.internal.util.slim.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
@@ -200,6 +205,40 @@ public class QuickSettingsUtil {
 
     }
 
+    public static ArrayList<String> getAllDynamicTiles(Context context) {
+         // Don't show the ime switcher if not supported
+        if (!DeviceUtils.deviceSupportsImeSwitcher(context)) {
+            DYNAMIC_TILES_DEFAULT.remove(TILE_IMESWITCHER);
+        }
+        // Don't show the usb tethering tile if not supported
+        if (!DeviceUtils.deviceSupportsUsbTether(context)) {
+            DYNAMIC_TILES_DEFAULT.remove(TILE_USBTETHER);
+        }
+        return DYNAMIC_TILES_DEFAULT;
+    }
+
+    public static String getDynamicTileDescription(Context context, String tile) {
+        if (tile.equals(TILE_IMESWITCHER)) {
+            return context.getResources().getString(R.string.dynamic_tile_ime_switcher);
+        } else if (tile.equals(TILE_USBTETHER)) {
+            return context.getResources().getString(R.string.dynamic_tile_usb_tether);
+        } else if (tile.equals(TILE_ALARM)) {
+            return context.getResources().getString(R.string.dynamic_tile_alarm);
+        } else if (tile.equals(TILE_BUGREPORT)) {
+            return context.getResources().getString(R.string.dynamic_tile_bugreport);
+        }
+        return null;
+    }
+
+    public static boolean[] toPrimitiveArray(ArrayList<Boolean> booleanList) {
+        boolean[] primitives = new boolean[booleanList.size()];
+        int index = 0;
+        for (Boolean value : booleanList) {
+            primitives[index++] = value;
+        }
+        return primitives;
+    }
+
     private static synchronized void refreshAvailableTiles(Context context) {
         ContentResolver resolver = context.getContentResolver();
 
@@ -280,7 +319,7 @@ public class QuickSettingsUtil {
     }
 
     public static ArrayList<String> getTileListFromString(String tiles) {
-        return new ArrayList<String>(Arrays.asList(tiles.split("\\|")));
+        return new ArrayList<String>(Arrays.asList(tiles.split("\\" + TILE_DELIMITER)));
     }
 
     public static String getTileStringFromList(ArrayList<String> tiles) {

wantowan2/wantowan2-android_packages_apps_Settings

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java

src/com/android/settings/beanstalk/quicksettings/QuickSettingsTiles.java
@@ -16,6 +16,8 @@
 
 package com.android.settings.beanstalk.quicksettings;
 
+import static com.android.internal.util.beanstalk.QSConstants.TILE_DELIMITER;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -62,8 +64,9 @@ import java.util.Comparator;
 
 public class QuickSettingsTiles extends Fragment {
 
-    private static final int MENU_RESET = Menu.FIRST;
-    private static final int MENU_HELP  = MENU_RESET + 1;
+    private static final int MENU_RESET         = Menu.FIRST;
+    private static final int MENU_DYNAMICTILES  = MENU_RESET + 1;
+    private static final int MENU_HELP          = MENU_DYNAMICTILES + 1;
 
     private static final String SEPARATOR = "OV=I=XseparatorX=I=VO";
 
@@ -75,6 +78,7 @@ public class QuickSettingsTiles extends Fragment {
     private static final int DLG_SHOW_LIST     = 5;
     private static final int DLG_HELP          = 6;
     private static final int DLG_DISABLED      = 7;
+    private static final int DLG_DYNAMICTILES  = 8;
 
     private DraggableGridView mDragView;
     private ViewGroup mContainer;
@@ -314,6 +318,9 @@ public class QuickSettingsTiles extends Fragment {
         menu.add(0, MENU_RESET, 0, R.string.reset)
                 .setIcon(R.drawable.ic_settings_backup) // use the backup icon
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0, MENU_DYNAMICTILES, 0, R.string.dynamic_tiles_title)
+                .setIcon(R.drawable.ic_settings_dynamic_tiles)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         menu.add(0, MENU_HELP, 0, R.string.help_label)
                 .setIcon(R.drawable.ic_settings_about)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
@@ -324,6 +331,9 @@ public class QuickSettingsTiles extends Fragment {
         switch (item.getItemId()) {
             case MENU_RESET:
                 showDialogInner(DLG_RESET);
+                return true;
+            case MENU_DYNAMICTILES:
+                showDialogInner(DLG_DYNAMICTILES);
                 return true;
             case MENU_HELP:
                 showDialogInner(DLG_HELP);
@@ -578,6 +588,68 @@ public class QuickSettingsTiles extends Fragment {
                                 QuickSettingsUtil.TILES.get(
                                     getOwner().mTileAdapter.getTileId(position));
                             getOwner().addTile(info.getTitleResId(), info.getIcon(), 0, true);
+                        }
+                    })
+                    .create();
+                case DLG_DYNAMICTILES:
+                    final ArrayList<String> allTiles =
+                        QuickSettingsUtil.getAllDynamicTiles(getActivity());
+                    String dynamicTiles = Settings.System.getStringForUser(
+                            getActivity().getContentResolver(),
+                            Settings.System.QUICK_SETTINGS_DYNAMIC_TILES,
+                            UserHandle.USER_CURRENT);
+                    if (dynamicTiles == null) {
+                        // default all dynamic tiles are turned on
+                        dynamicTiles = TextUtils.join(TILE_DELIMITER, allTiles);
+                    }
+
+                    final ArrayList<String> actualEntries = new ArrayList<String>();
+                    final ArrayList<Boolean> actualTilesStatus = new ArrayList<Boolean>();
+
+                    boolean detected;
+                    for (String tile : allTiles) {
+                        detected = false;
+                        for (String actualTile : dynamicTiles.split(
+                                "\\" + TILE_DELIMITER)) {
+                            if (tile.equals(actualTile)) {
+                                detected = true;
+                            }
+                        }
+                        actualTilesStatus.add(detected);
+                        actualEntries.add(
+                            QuickSettingsUtil.getDynamicTileDescription(getActivity(), tile));
+                    }
+
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.dynamic_tiles_title)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setMultiChoiceItems(
+                        actualEntries.toArray(new String[actualEntries.size()]),
+                        QuickSettingsUtil.toPrimitiveArray(actualTilesStatus),
+                        new  DialogInterface.OnMultiChoiceClickListener() {
+                        public void onClick(DialogInterface dialog, int indexSelected,
+                                boolean isChecked) {
+                            actualTilesStatus.set(indexSelected, isChecked);
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            String savedTiles = "";
+                            for (int i = 0; i < actualTilesStatus.size(); i++) {
+                                if (actualTilesStatus.get(i)) {
+                                    savedTiles += allTiles.get(i);
+                                    savedTiles += TILE_DELIMITER;
+                                }
+                            }
+                            if (!savedTiles.isEmpty()) {
+                                savedTiles = savedTiles.substring(0, savedTiles.length() - 1);
+                            }
+                            Settings.System.putString(
+                                getActivity().getContentResolver(),
+                                Settings.System.QUICK_SETTINGS_DYNAMIC_TILES,
+                                savedTiles);
                         }
                     })
                     .create();

src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java
@@ -18,13 +18,17 @@
 package com.android.settings.beanstalk.quicksettings;
 
 import static com.android.internal.util.beanstalk.QSConstants.TILES_DEFAULT;
+import static com.android.internal.util.beanstalk.QSConstants.DYNAMIC_TILES_DEFAULT;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_AIRPLANE;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_ALARM;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_AUTOROTATE;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_BATTERY;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_BRIGHTNESS;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_BUGREPORT;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_DELIMITER;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_EXPANDEDDESKTOP;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_IMESWITCHER;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_LOCATION;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_LTE;
@@ -39,6 +43,7 @@ import static com.android.internal.util.beanstalk.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_SYNC;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_TORCH;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_USER;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_WIFI;
@@ -200,6 +205,40 @@ public class QuickSettingsUtil {
 
     }
 
+    public static ArrayList<String> getAllDynamicTiles(Context context) {
+         // Don't show the ime switcher if not supported
+        if (!DeviceUtils.deviceSupportsImeSwitcher(context)) {
+            DYNAMIC_TILES_DEFAULT.remove(TILE_IMESWITCHER);
+        }
+        // Don't show the usb tethering tile if not supported
+        if (!DeviceUtils.deviceSupportsUsbTether(context)) {
+            DYNAMIC_TILES_DEFAULT.remove(TILE_USBTETHER);
+        }
+        return DYNAMIC_TILES_DEFAULT;
+    }
+
+    public static String getDynamicTileDescription(Context context, String tile) {
+        if (tile.equals(TILE_IMESWITCHER)) {
+            return context.getResources().getString(R.string.dynamic_tile_ime_switcher);
+        } else if (tile.equals(TILE_USBTETHER)) {
+            return context.getResources().getString(R.string.dynamic_tile_usb_tether);
+        } else if (tile.equals(TILE_ALARM)) {
+            return context.getResources().getString(R.string.dynamic_tile_alarm);
+        } else if (tile.equals(TILE_BUGREPORT)) {
+            return context.getResources().getString(R.string.dynamic_tile_bugreport);
+        }
+        return null;
+    }
+
+    public static boolean[] toPrimitiveArray(ArrayList<Boolean> booleanList) {
+        boolean[] primitives = new boolean[booleanList.size()];
+        int index = 0;
+        for (Boolean value : booleanList) {
+            primitives[index++] = value;
+        }
+        return primitives;
+    }
+
     private static synchronized void refreshAvailableTiles(Context context) {
         ContentResolver resolver = context.getContentResolver();
 
@@ -280,7 +319,7 @@ public class QuickSettingsUtil {
     }
 
     public static ArrayList<String> getTileListFromString(String tiles) {
-        return new ArrayList<String>(Arrays.asList(tiles.split("\\|")));
+        return new ArrayList<String>(Arrays.asList(tiles.split("\\" + TILE_DELIMITER)));
     }
 
     public static String getTileStringFromList(ArrayList<String> tiles) {