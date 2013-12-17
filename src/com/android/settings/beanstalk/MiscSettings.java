a8475a8f96e8c5f9fd185e87ed7894c9d0c915d3
scott <scott@ubuntu>
2013-12-17 00:18:13 -0500

    remove old density changer and use new on-the-fly changer. Thanks slim.
    Change-Id: Ifa0daf3e142f1b631b048a5196102de13eb9b510
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/beanstalk/MiscSettings.java
+++ src/com/android/settings/beanstalk/MiscSettings.java
@@ -16,7 +16,17 @@
 
 package com.android.settings.beanstalk;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.content.ContentResolver;
 import android.provider.Settings;
@@ -26,29 +36,44 @@ import android.preference.Preference;
 import android.content.DialogInterface;
 import android.view.WindowManagerGlobal;
 import android.content.Intent;
-import android.preference.PreferenceScreen;
 import android.os.RemoteException;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
-import android.preference.CheckBoxPreference;
-import android.preference.PreferenceCategory;
+import android.preference.ListPreference;
 import android.text.Spannable;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
+import android.text.Editable;
+import android.util.Slog;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
-
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import java.util.List;
+import java.lang.Thread;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MiscSettings extends SettingsPreferenceFragment
         implements OnPreferenceChangeListener {
 
+    private static final String TAG = "MiscSettings";
     private static final String PREF_MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+    private static final String KEY_LCD_DENSITY = "lcd_density";
+    private static final int DIALOG_CUSTOM_DENSITY = 101;
+    private static final String DENSITY_PROP = "persist.sys.lcd_density";
+
+    private static ListPreference mLcdDensity;
+    private static Activity mActivity;
 
     private ListPreference mMsob;
     private Preference mCustomLabel;
@@ -57,7 +82,13 @@ public class MiscSettings extends SettingsPreferenceFragment
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	mActivity = getActivity();
 
+        updateSettings();
+    }
+
+    private void updateSettings() {
+        setPreferenceScreen(null);
         addPreferencesFromResource(R.xml.misc_settings);
 	PreferenceScreen prefScreen = getPreferenceScreen();
 
@@ -69,6 +100,19 @@ public class MiscSettings extends SettingsPreferenceFragment
         mMsob.setOnPreferenceChangeListener(this);
 
 	updateCustomLabelTextSummary();
+
+	mLcdDensity = (ListPreference) findPreference(KEY_LCD_DENSITY);
+        String current = SystemProperties.get(DENSITY_PROP,
+                SystemProperties.get("ro.sf.lcd_density"));
+        final ArrayList<String> array = new ArrayList<String>(
+                Arrays.asList(getResources().getStringArray(R.array.lcd_density_entries)));
+        if (array.contains(current)) {
+            mLcdDensity.setValue(current);
+        } else {
+            mLcdDensity.setValue("custom");
+        }
+        mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + current);
+        mLcdDensity.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -81,6 +125,16 @@ public class MiscSettings extends SettingsPreferenceFragment
 
             mMsob.setValue(String.valueOf(value));
             mMsob.setSummary(mMsob.getEntry());
+            return true;
+	} else if (preference == mLcdDensity) {
+            String density = (String) newValue;
+            if (SystemProperties.get(DENSITY_PROP) != density) {
+                if ((density).equals(getResources().getString(R.string.custom_density))) {
+                    showDialogInner(DIALOG_CUSTOM_DENSITY);
+                } else {
+                    setDensity(Integer.parseInt(density));
+                }
+            }
             return true;
         }
         return false;
@@ -130,4 +184,102 @@ public class MiscSettings extends SettingsPreferenceFragment
             mCustomLabel.setSummary(mCustomLabelText);
         }
      }
+
+    private static void setDensity(int density) {
+        int max = mActivity.getResources().getInteger(R.integer.lcd_density_max);
+        int min = mActivity.getResources().getInteger(R.integer.lcd_density_min);
+        int navbarHeight = Settings.System.getIntForUser(mActivity.getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, mActivity.getResources()
+                .getDimensionPixelSize(com.android.internal.R.dimen.navigation_bar_height),
+                UserHandle.USER_CURRENT);
+        if (density < min && density > max) {
+            mLcdDensity.setSummary(mActivity.getResources().getString(
+                                            R.string.custom_density_summary_invalid));
+        }
+        SystemProperties.set(DENSITY_PROP, Integer.toString(density));
+        Settings.System.putInt(mActivity.getContentResolver(),
+                Settings.System.LCD_DENSITY, density);
+
+        killCurrentLauncher();
+        Configuration mConfiguration = new Configuration();
+        mConfiguration.setToDefaults();
+        try {
+            ActivityManagerNative.getDefault().updateConfiguration(mConfiguration);
+        } catch (RemoteException e) {
+            Slog.w(TAG, "Failure communicating with activity manager", e);
+        }
+        mActivity.recreate();
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e){}
+        Settings.System.putInt(mActivity.getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, navbarHeight);
+    }
+
+    private static void killCurrentLauncher() {
+        ComponentName defaultLauncher = mActivity.getPackageManager().getHomeActivities(
+                        new ArrayList<ResolveInfo>());
+                ActivityManager am = (ActivityManager) mActivity.getSystemService(
+                        Context.ACTIVITY_SERVICE);
+                am.killBackgroundProcesses(defaultLauncher.getPackageName());
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        InterfaceSettings getOwner() {
+            return (InterfaceSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            LayoutInflater factory = LayoutInflater.from(getActivity());
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DIALOG_CUSTOM_DENSITY:
+                    final View textEntryView = factory.inflate(
+                            R.layout.alert_dialog_text_entry, null);
+                    return new AlertDialog.Builder(getActivity())
+                            .setTitle(getResources().getString(R.string.set_custom_density_title))
+                            .setView(textEntryView)
+                            .setPositiveButton(getResources().getString(
+                                    R.string.set_custom_density_set),
+                                    new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    EditText dpi = (EditText)
+                                            textEntryView.findViewById(R.id.dpi_edit);
+                                    Editable text = dpi.getText();
+                                    dialog.dismiss();
+                                    setDensity(Integer.parseInt(text.toString()));
+
+                                }
+                            })
+                            .setNegativeButton(getResources().getString(R.string.cancel),
+                                    new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+        }
+    }
 }
