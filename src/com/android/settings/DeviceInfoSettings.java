ResurrectionRemix/Resurrection_packages_apps_Settings

Nyan nyan nyan nyan

src/com/android/settings/DeviceInfoSettings.java
@@ -87,6 +87,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_VERSION, "ro.pa.version");
+        findPreference(KEY_MOD_VERSION).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
@@ -169,6 +170,19 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.setClassName("android",
                         com.android.internal.app.PlatLogoActivity.class.getName());
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
+        } else if (preference.getKey().equals(KEY_MOD_VERSION)) {
+            System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
+            mHits[mHits.length-1] = SystemClock.uptimeMillis();
+            if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("com.android.settings",
+                        com.android.settings.paranoid.PlatLogoActivity.class.getName());
                 try {
                     startActivity(intent);
                 } catch (Exception e) {

src/com/android/settings/paranoid/Nyandroid.java
@@ -1,0 +1,247 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.paranoid;
+
+import android.animation.AnimatorSet;
+import android.animation.PropertyValuesHolder;
+import android.animation.ObjectAnimator;
+import android.animation.TimeAnimator;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Pair;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import java.util.HashMap;
+import java.util.Random;
+
+import com.android.settings.R;
+
+public class Nyandroid extends Activity {
+    final static boolean DEBUG = false;
+
+    public static class Board extends FrameLayout
+    {
+        public static final boolean FIXED_STARS = true;
+        public static final int NUM_CATS = 20;
+
+        static Random sRNG = new Random();
+
+        static float lerp(float a, float b, float f) {
+            return (b-a)*f + a;
+        }
+
+        static float randfrange(float a, float b) {
+            return lerp(a, b, sRNG.nextFloat());
+        }
+
+        static int randsign() {
+            return sRNG.nextBoolean() ? 1 : -1;
+        }
+
+        static <E> E pick(E[] array) {
+            if (array.length == 0) return null;
+            return array[sRNG.nextInt(array.length)];
+        }
+
+        public class FlyingCat extends ImageView {
+            public static final float VMAX = 1000.0f;
+            public static final float VMIN = 100.0f;
+
+            public float v, vr;
+
+            public float dist;
+            public float z;
+
+            public ComponentName component;
+
+            public FlyingCat(Context context, AttributeSet as) {
+                super(context, as);
+                setImageResource(R.drawable.nyandroid_anim); // @@@
+
+                if (DEBUG) setBackgroundColor(0x80FF0000);
+            }
+
+            public String toString() {
+                return String.format("<cat (%.1f, %.1f) (%d x %d)>",
+                    getX(), getY(), getWidth(), getHeight());
+            }
+
+            public void reset() {
+                final float scale = lerp(0.1f,2f,z);
+                setScaleX(scale); setScaleY(scale);
+
+                setX(-scale*getWidth()+1);
+                setY(randfrange(0, Board.this.getHeight()-scale*getHeight()));
+                v = lerp(VMIN, VMAX, z);
+
+                dist = 0;
+            }
+
+            public void update(float dt) {
+                dist += v * dt;
+                setX(getX() + v * dt);
+            }
+        }
+
+        TimeAnimator mAnim;
+
+        public Board(Context context, AttributeSet as) {
+            super(context, as);
+
+            setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            setBackgroundColor(0xFF003366);
+        }
+
+        private void reset() {
+            removeAllViews();
+
+            final ViewGroup.LayoutParams wrap = new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            if (FIXED_STARS) {
+                for(int i=0; i<20; i++) {
+                    ImageView fixedStar = new ImageView(getContext(), null);
+                    if (DEBUG) fixedStar.setBackgroundColor(0x8000FF80);
+                    fixedStar.setImageResource(R.drawable.star_anim); // @@@
+                    addView(fixedStar, wrap);
+                    final float scale = randfrange(0.1f, 1f);
+                    fixedStar.setScaleX(scale); fixedStar.setScaleY(scale);
+                    fixedStar.setX(randfrange(0, getWidth()));
+                    fixedStar.setY(randfrange(0, getHeight()));
+                    final AnimationDrawable anim = (AnimationDrawable) fixedStar.getDrawable();
+                    postDelayed(new Runnable() { 
+                        public void run() {
+                            anim.start();
+                        }}, (int) randfrange(0, 1000));
+                }
+            }
+
+            for(int i=0; i<NUM_CATS; i++) {
+                FlyingCat nv = new FlyingCat(getContext(), null);
+                addView(nv, wrap);
+                nv.z = ((float)i/NUM_CATS);
+                nv.z *= nv.z;
+                nv.reset();
+                nv.setX(randfrange(0,Board.this.getWidth()));
+                final AnimationDrawable anim = (AnimationDrawable) nv.getDrawable();
+                postDelayed(new Runnable() { 
+                    public void run() {
+                        anim.start();
+                    }}, (int) randfrange(0, 1000));
+            }
+
+            if (mAnim != null) {
+                mAnim.cancel();
+            }
+            mAnim = new TimeAnimator();
+            mAnim.setTimeListener(new TimeAnimator.TimeListener() {
+                public void onTimeUpdate(TimeAnimator animation, long totalTime, long deltaTime) {
+                    for (int i=0; i<getChildCount(); i++) {
+                        View v = getChildAt(i);
+                        if (!(v instanceof FlyingCat)) continue;
+                        FlyingCat nv = (FlyingCat) v;
+                        nv.update(deltaTime / 1000f);
+                        final float catWidth = nv.getWidth() * nv.getScaleX();
+                        final float catHeight = nv.getHeight() * nv.getScaleY();
+                        if (   nv.getX() + catWidth < -2
+                            || nv.getX() > getWidth() + 2
+                            || nv.getY() + catHeight < -2
+                            || nv.getY() > getHeight() + 2)
+                        {
+                            nv.reset();
+                        }
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void onSizeChanged (int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w,h,oldw,oldh);
+            post(new Runnable() { public void run() { 
+                reset();
+                mAnim.start(); 
+            } });
+        }
+
+
+        @Override
+        protected void onDetachedFromWindow() {
+            super.onDetachedFromWindow();
+            mAnim.cancel();
+        }
+
+        @Override
+        public boolean isOpaque() {
+            return true;
+        }
+    }
+
+    private Board mBoard;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getWindow().addFlags(
+                  WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                );
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBoard = new Board(this, null);
+        setContentView(mBoard);
+
+        mBoard.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int vis) {
+                if (0 == (vis & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)) {
+                    Nyandroid.this.finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onUserInteraction() {
+        finish();
+    }
+}

src/com/android/settings/paranoid/PlatLogoActivity.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.paranoid;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class PlatLogoActivity extends Activity {
+    Toast mToast;
+    ImageView mContent;
+    Vibrator mZzz;
+    int mCount;
+    final Handler mHandler = new Handler();
+
+    Runnable mSuperLongPress = new Runnable() {
+        public void run() {
+            mCount++;
+            mZzz.vibrate(50 * mCount);
+            final float scale = 1f + 0.25f * mCount * mCount;
+            mContent.setScaleX(scale);
+            mContent.setScaleY(scale);
+
+            if (mCount <= 3) {
+                mHandler.postDelayed(mSuperLongPress, ViewConfiguration.getLongPressTimeout());
+            } else {
+                try {
+                    startActivity(new Intent(Intent.ACTION_MAIN)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                            | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                        .setClassName("com.android.settings","com.android.settings.paranoid.Nyandroid"));
+                } catch (ActivityNotFoundException ex) {
+                    android.util.Log.e("PlatLogoActivity", "Couldn't find platlogo screensaver.");
+                }
+                finish();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        mToast = Toast.makeText(this, "ParanoidAndroid", Toast.LENGTH_SHORT);
+
+        mZzz = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        mContent = new ImageView(this);
+        mContent.setImageResource(com.android.internal.R.drawable.platlogo);
+        mContent.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        mContent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int action = event.getAction();
+                if (action == MotionEvent.ACTION_DOWN) {
+                    mContent.setPressed(true);
+                    mHandler.removeCallbacks(mSuperLongPress);
+                    mCount = 0;
+                    mHandler.postDelayed(mSuperLongPress, 2*ViewConfiguration.getLongPressTimeout());
+                } else if (action == MotionEvent.ACTION_UP) {
+                    if (mContent.isPressed()) {
+                        mContent.setPressed(false);
+                        mHandler.removeCallbacks(mSuperLongPress);
+                        mToast.show();
+                    }
+                }
+                return true;
+            }
+        });
+        
+        setContentView(mContent);
+    }
+}

wantowan2/wantowan2-android_packages_apps_Settings

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	AndroidManifest.xml
	src/com/android/settings/DeviceInfoSettings.java
Change-Id: I9c617e0b155232392134f62ffaaf45f5c2690cea

src/com/android/settings/DeviceInfoSettings.java
@@ -105,6 +105,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
  //       findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_VERSION, "ro.cm.version");
+        findPreference(KEY_MOD_VERSION).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
@@ -210,6 +211,19 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.setClassName("android",
                         com.android.internal.app.PlatLogoActivity.class.getName());
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
+        } else if (preference.getKey().equals(KEY_MOD_VERSION)) {
+            System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
+            mHits[mHits.length-1] = SystemClock.uptimeMillis();
+            if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("com.android.settings",
+                        com.android.settings.beanstalk.PlatLogoActivity.class.getName());
                 try {
                     startActivity(intent);
                 } catch (Exception e) {

src/com/android/settings/beanstalk/Nyandroid.java
@@ -1,0 +1,247 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk;
+
+import android.animation.AnimatorSet;
+import android.animation.PropertyValuesHolder;
+import android.animation.ObjectAnimator;
+import android.animation.TimeAnimator;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Pair;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import java.util.HashMap;
+import java.util.Random;
+
+import com.android.settings.R;
+
+public class Nyandroid extends Activity {
+    final static boolean DEBUG = false;
+
+    public static class Board extends FrameLayout
+    {
+        public static final boolean FIXED_STARS = true;
+        public static final int NUM_CATS = 20;
+
+        static Random sRNG = new Random();
+
+        static float lerp(float a, float b, float f) {
+            return (b-a)*f + a;
+        }
+
+        static float randfrange(float a, float b) {
+            return lerp(a, b, sRNG.nextFloat());
+        }
+
+        static int randsign() {
+            return sRNG.nextBoolean() ? 1 : -1;
+        }
+
+        static <E> E pick(E[] array) {
+            if (array.length == 0) return null;
+            return array[sRNG.nextInt(array.length)];
+        }
+
+        public class FlyingCat extends ImageView {
+            public static final float VMAX = 1000.0f;
+            public static final float VMIN = 100.0f;
+
+            public float v, vr;
+
+            public float dist;
+            public float z;
+
+            public ComponentName component;
+
+            public FlyingCat(Context context, AttributeSet as) {
+                super(context, as);
+                setImageResource(R.drawable.nyandroid_anim); // @@@
+
+                if (DEBUG) setBackgroundColor(0x80FF0000);
+            }
+
+            public String toString() {
+                return String.format("<cat (%.1f, %.1f) (%d x %d)>",
+                    getX(), getY(), getWidth(), getHeight());
+            }
+
+            public void reset() {
+                final float scale = lerp(0.1f,2f,z);
+                setScaleX(scale); setScaleY(scale);
+
+                setX(-scale*getWidth()+1);
+                setY(randfrange(0, Board.this.getHeight()-scale*getHeight()));
+                v = lerp(VMIN, VMAX, z);
+
+                dist = 0;
+            }
+
+            public void update(float dt) {
+                dist += v * dt;
+                setX(getX() + v * dt);
+            }
+        }
+
+        TimeAnimator mAnim;
+
+        public Board(Context context, AttributeSet as) {
+            super(context, as);
+
+            setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            setBackgroundColor(0xFF003366);
+        }
+
+        private void reset() {
+            removeAllViews();
+
+            final ViewGroup.LayoutParams wrap = new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            if (FIXED_STARS) {
+                for(int i=0; i<20; i++) {
+                    ImageView fixedStar = new ImageView(getContext(), null);
+                    if (DEBUG) fixedStar.setBackgroundColor(0x8000FF80);
+                    fixedStar.setImageResource(R.drawable.star_anim); // @@@
+                    addView(fixedStar, wrap);
+                    final float scale = randfrange(0.1f, 1f);
+                    fixedStar.setScaleX(scale); fixedStar.setScaleY(scale);
+                    fixedStar.setX(randfrange(0, getWidth()));
+                    fixedStar.setY(randfrange(0, getHeight()));
+                    final AnimationDrawable anim = (AnimationDrawable) fixedStar.getDrawable();
+                    postDelayed(new Runnable() { 
+                        public void run() {
+                            anim.start();
+                        }}, (int) randfrange(0, 1000));
+                }
+            }
+
+            for(int i=0; i<NUM_CATS; i++) {
+                FlyingCat nv = new FlyingCat(getContext(), null);
+                addView(nv, wrap);
+                nv.z = ((float)i/NUM_CATS);
+                nv.z *= nv.z;
+                nv.reset();
+                nv.setX(randfrange(0,Board.this.getWidth()));
+                final AnimationDrawable anim = (AnimationDrawable) nv.getDrawable();
+                postDelayed(new Runnable() { 
+                    public void run() {
+                        anim.start();
+                    }}, (int) randfrange(0, 1000));
+            }
+
+            if (mAnim != null) {
+                mAnim.cancel();
+            }
+            mAnim = new TimeAnimator();
+            mAnim.setTimeListener(new TimeAnimator.TimeListener() {
+                public void onTimeUpdate(TimeAnimator animation, long totalTime, long deltaTime) {
+                    for (int i=0; i<getChildCount(); i++) {
+                        View v = getChildAt(i);
+                        if (!(v instanceof FlyingCat)) continue;
+                        FlyingCat nv = (FlyingCat) v;
+                        nv.update(deltaTime / 1000f);
+                        final float catWidth = nv.getWidth() * nv.getScaleX();
+                        final float catHeight = nv.getHeight() * nv.getScaleY();
+                        if (   nv.getX() + catWidth < -2
+                            || nv.getX() > getWidth() + 2
+                            || nv.getY() + catHeight < -2
+                            || nv.getY() > getHeight() + 2)
+                        {
+                            nv.reset();
+                        }
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void onSizeChanged (int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w,h,oldw,oldh);
+            post(new Runnable() { public void run() { 
+                reset();
+                mAnim.start(); 
+            } });
+        }
+
+
+        @Override
+        protected void onDetachedFromWindow() {
+            super.onDetachedFromWindow();
+            mAnim.cancel();
+        }
+
+        @Override
+        public boolean isOpaque() {
+            return true;
+        }
+    }
+
+    private Board mBoard;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getWindow().addFlags(
+                  WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                );
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBoard = new Board(this, null);
+        setContentView(mBoard);
+
+        mBoard.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int vis) {
+                if (0 == (vis & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)) {
+                    Nyandroid.this.finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onUserInteraction() {
+        finish();
+    }
+}

src/com/android/settings/beanstalk/PlatLogoActivity.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class PlatLogoActivity extends Activity {
+    Toast mToast;
+    ImageView mContent;
+    Vibrator mZzz;
+    int mCount;
+    final Handler mHandler = new Handler();
+
+    Runnable mSuperLongPress = new Runnable() {
+        public void run() {
+            mCount++;
+            mZzz.vibrate(50 * mCount);
+            final float scale = 1f + 0.25f * mCount * mCount;
+            mContent.setScaleX(scale);
+            mContent.setScaleY(scale);
+
+            if (mCount <= 3) {
+                mHandler.postDelayed(mSuperLongPress, ViewConfiguration.getLongPressTimeout());
+            } else {
+                try {
+                    startActivity(new Intent(Intent.ACTION_MAIN)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                            | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                        .setClassName("com.android.settings","com.android.settings.beanstalk.Nyandroid"));
+                } catch (ActivityNotFoundException ex) {
+                    android.util.Log.e("PlatLogoActivity", "Couldn't find platlogo screensaver.");
+                }
+                finish();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        mToast = Toast.makeText(this, "BeanStalk", Toast.LENGTH_SHORT);
+
+        mZzz = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        mContent = new ImageView(this);
+        mContent.setImageResource(com.android.internal.R.drawable.platlogo);
+        mContent.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        mContent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int action = event.getAction();
+                if (action == MotionEvent.ACTION_DOWN) {
+                    mContent.setPressed(true);
+                    mHandler.removeCallbacks(mSuperLongPress);
+                    mCount = 0;
+                    mHandler.postDelayed(mSuperLongPress, 2*ViewConfiguration.getLongPressTimeout());
+                } else if (action == MotionEvent.ACTION_UP) {
+                    if (mContent.isPressed()) {
+                        mContent.setPressed(false);
+                        mHandler.removeCallbacks(mSuperLongPress);
+                        mToast.show();
+                    }
+                }
+                return true;
+            }
+        });
+        
+        setContentView(mContent);
+    }
+}