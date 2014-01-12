02ad9336f3da8631c5bad0c007616a34ced248db
ILWT <tsubus@lagat.org>
2014-01-12 13:50:22 +0100

    Add Fast Charge QS Tile [2/2]
    Change-Id: Iafd5ee0eefa9f7abd41b03ea7fa4e976c573e61c
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	res/values/custom_strings.xml
    	src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
    
    Conflicts:
    	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
    
    Conflicts:
    	src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java

--- src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java
@@ -55,6 +55,7 @@ import static com.android.internal.util.beanstalk.QSConstants.TILE_WIFI;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_INTERNALMEMORY;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_FCHARGE;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -170,6 +171,9 @@ public class QuickSettingsUtil {
 	registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_INTERNALMEMORY, R.string.internal_memory_tile,
                 "com.android.systemui:drawable/ic_qs_internalmemory"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_FCHARGE, R.string.title_tile_fcharge,
+                "com.android.systemui:drawable/ic_qs_fcharge_off"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -220,6 +224,11 @@ public class QuickSettingsUtil {
         // Don't show the Torch tile if not supported
         if (!DeviceUtils.deviceSupportsTorch(context)) {
             removeTile(TILE_TORCH);
+        }
+
+        // Don't show the Fast charge tile if not supported by kernel
+        if (!DeviceUtils.fchargeEnabled(context)) {
+            removeTile(TILE_FCHARGE);
         }
 
     }
