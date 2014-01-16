dee281c50396c4092f678bc507888f1384ebdbf6
scott <scott@ubuntu>
2014-01-16 00:09:57 -0500

    update for BS
    Change-Id: Ia23b5f9df221903ae0c9327d1828011ff8dfc695
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/beanstalk/CustomCarrier.java
+++ src/com/android/settings/beanstalk/CustomCarrier.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.slim;
+package com.android.settings.beanstalk;
 
 import android.content.Context;
 import android.preference.EditTextPreference;
