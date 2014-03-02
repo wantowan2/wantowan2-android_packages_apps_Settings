385e9b5d5bebedc0f5ffeab315182d2ad70a8b6e
scott <scott@ubuntu.com>
2014-03-02 02:46:59 -0500

    lockscreen wallpaper option
    Change-Id: Iaed099b3f37eac3b5f096093e383ecf382342d23
    Signed-off-by: scott <scott@ubuntu.com>

--- src/com/android/settings/beanstalk/LockscreenMisc.java
+++ src/com/android/settings/beanstalk/LockscreenMisc.java
@@ -20,7 +20,15 @@ import android.app.ActivityManager;
 import android.app.admin.DeviceAdminReceiver;
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ActivityNotFoundException;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.net.Uri;
 import android.os.UserHandle;
 import android.content.ContentResolver;
 import android.preference.PreferenceGroup;
@@ -40,7 +48,22 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import java.io.File;
+import java.io.IOException;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.provider.MediaStore;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 public class LockscreenMisc extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+    private static final String TAG = "LockscreenMisc";
 
     private static final String KEY_ALLOW_ROTATION = "allow_rotation";
     private static final String KEY_SEE_TRHOUGH = "see_through";
@@ -52,6 +75,22 @@ public class LockscreenMisc extends SettingsPreferenceFragment implements OnPref
     private static final String MENU_UNLOCK_SCREEN = "menu_unlock_screen";
     private static final String CAMERA_UNLOCK_SCREEN = "camera_unlock_screen";
     private static final String KEY_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+    private static final String LOCKSCREEN_BACKGROUND = "lockscreen_background";
+    private static final String WALLPAPER_NAME = "lockscreen_wallpaper";
+    private static final String LOCKSCREEN_BACKGROUND_STYLE = "lockscreen_background_style";
+    private static final String LOCKSCREEN_BACKGROUND_COLOR_FILL = "lockscreen_background_color_fill";
+
+    private static final int REQUEST_PICK_WALLPAPER = 201;
+    private static final int COLOR_FILL = 0;
+    private static final int CUSTOM_IMAGE = 1;
+    private static final int DEFAULT = 2;
+
+    private ColorPickerPreference mLockColorFill;
+    private ListPreference mLockBackground;
+
+    private PreferenceCategory mLockscreenBackground;
+    private File wallpaperImage;
+    private File wallpaperTemporary;
 
     private CheckBoxPreference mSeeThrough;
     private ListPreference mBatteryStatus;
@@ -137,6 +176,21 @@ public class LockscreenMisc extends SettingsPreferenceFragment implements OnPref
             prefScreen.removePreference(mCameraUnlock);
         }
 
+	mLockscreenBackground = (PreferenceCategory) findPreference(LOCKSCREEN_BACKGROUND);
+
+        mLockBackground = (ListPreference) findPreference(LOCKSCREEN_BACKGROUND_STYLE);
+        mLockBackground.setOnPreferenceChangeListener(this);
+        mLockBackground.setValue(Integer.toString(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2)));
+        mLockBackground.setSummary(mLockBackground.getEntry());
+
+        mLockColorFill = (ColorPickerPreference) findPreference(LOCKSCREEN_BACKGROUND_COLOR_FILL);
+        mLockColorFill.setOnPreferenceChangeListener(this);
+        mLockColorFill.setSummary(ColorPickerPreference.convertToARGB(
+                Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_BACKGROUND_COLOR, 0x00000000)));
+
+	updateVisiblePreferences();
     }
 
     @Override
@@ -218,6 +272,18 @@ public class LockscreenMisc extends SettingsPreferenceFragment implements OnPref
             Settings.System.putInt(cr, Settings.System.LOCKSCREEN_BATTERY_VISIBILITY, value);
             mBatteryStatus.setSummary(mBatteryStatus.getEntries()[index]);
             return true;
+	} else if (preference == mLockBackground) {
+            int index = mLockBackground.findIndexOfValue(String.valueOf(objValue));
+            preference.setSummary(mLockBackground.getEntries()[index]);
+            return handleBackgroundSelection(index);
+        } else if (preference == mLockColorFill) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int color = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_BACKGROUND_COLOR, color);
+            return true;
         }
 
          return false;
@@ -225,4 +291,121 @@ public class LockscreenMisc extends SettingsPreferenceFragment implements OnPref
 
     public static class DeviceAdminLockscreenReceiver extends DeviceAdminReceiver {}
 
+    private void updateVisiblePreferences() {
+        int visible = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2);
+
+        if (visible == 0) {
+			mLockColorFill.setEnabled(true);
+        } else {
+			mLockColorFill.setEnabled(false);
+        }
+        if (visible != 2) {
+            mBlurBehind.setEnabled(false);
+            mBlurRadius.setEnabled(false);
+        } else {
+            mBlurBehind.setEnabled(true);
+            mBlurRadius.setEnabled(true);
+        }
+        if (visible != 1) {
+            mSeeThrough.setEnabled(true);
+        } else {
+            mSeeThrough.setEnabled(false);
+        }
+	}
+
+    private Uri getLockscreenExternalUri() {
+        File dir = getActivity().getExternalCacheDir();
+        File wallpaper = new File(dir, WALLPAPER_NAME);
+        return Uri.fromFile(wallpaper);
+	}
+
+    private boolean handleBackgroundSelection(int index) {
+        if (index == COLOR_FILL) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 0);
+            updateVisiblePreferences();
+            return true;
+        } else if (index == CUSTOM_IMAGE) {
+            // Used to reset the image when already set
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2);
+            // Launches intent for user to select an image/crop it to set as background
+            Display display = getActivity().getWindowManager().getDefaultDisplay();
+
+            int width = getActivity().getWallpaperDesiredMinimumWidth();
+            int height = getActivity().getWallpaperDesiredMinimumHeight();
+            float spotlightX = (float)display.getWidth() / width;
+            float spotlightY = (float)display.getHeight() / height;
+
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            intent.putExtra("crop", "true");
+            intent.putExtra("scale", true);
+            intent.putExtra("scaleUpIfNeeded", true);
+            intent.putExtra("aspectX", width);
+            intent.putExtra("aspectY", height);
+            intent.putExtra("outputX", width);
+            intent.putExtra("outputY", height);
+            intent.putExtra("spotlightX", spotlightX);
+            intent.putExtra("spotlightY", spotlightY);
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, getLockscreenExternalUri());
+
+            startActivityForResult(intent, REQUEST_PICK_WALLPAPER);
+        } else if (index == DEFAULT) {
+            // Sets background to default
+            Settings.System.putInt(getContentResolver(),
+                            Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2);
+            updateVisiblePreferences();
+            return true;
+        }
+        return false;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PICK_WALLPAPER) {
+            FileOutputStream wallpaperStream = null;
+            try {
+                wallpaperStream = getActivity().openFileOutput(WALLPAPER_NAME,
+                        Context.MODE_WORLD_READABLE);
+
+            } catch (FileNotFoundException e) {
+                return; // NOOOOO
+            }
+            Uri selectedImageUri = getLockscreenExternalUri();
+            Bitmap bitmap;
+            if (data != null) {
+                Uri mUri = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),
+                            mUri);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, wallpaperStream);
+
+                    Toast.makeText(getActivity(), getResources().getString(R.string.
+                            background_result_successful), Toast.LENGTH_LONG).show();
+                    Settings.System.putInt(getContentResolver(),
+                            Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 1);
+                    updateVisiblePreferences();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    bitmap = BitmapFactory.decodeFile(selectedImageUri.getPath());
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, wallpaperStream);
+                } catch (NullPointerException npe) {
+                    Log.e(TAG, "SeletedImageUri was null.");
+                    Toast.makeText(getActivity(), getResources().getString(R.string.
+                            background_result_not_successful), Toast.LENGTH_LONG).show();
+                    super.onActivityResult(requestCode, resultCode, data);
+                    return;
+                }
+            }
+        }
+    }
 }
