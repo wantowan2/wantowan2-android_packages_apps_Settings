46e457b2e2ab48368787d24f95796422ed281e1f
scott <scott@ubuntu.com>
2014-03-15 00:21:17 -0400

    Internal memory quick tile
    Change-Id: I361c468404a0430eb3b18475870026be6959c19f
    Signed-off-by: scott <scott@ubuntu.com>

--- src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java
@@ -54,6 +54,7 @@ import static com.android.internal.util.beanstalk.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_WIFI;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_INTERNALMEMORY;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -166,6 +167,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CONTACT, R.string.title_tile_contact,
                 "com.android.systemui:drawable/ic_qs_default_user"));
+	registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_INTERNALMEMORY, R.string.internal_memory_tile,
+                "com.android.systemui:drawable/ic_qs_internalmemory"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
