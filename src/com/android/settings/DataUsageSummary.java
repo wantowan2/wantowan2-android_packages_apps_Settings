szunyi77/android_packages_apps_Settings-1

[2/2] Squashed: Battery Saver mode
introduce battery saver mode
this based from what user want or what user do to saving the battery life
this include:
1. Automatically switch mobile network into what user choice and also disable mobile data IF wifi connected to an AP
2. Automatically switch mobile network and disable mobile data IF battery low (battery level can configure)
3. Reduce screen brightness based low battery (this feature will disabled if Auto brightness mode enabled)
4. Not changing network IF on Call or Usb tether enabled or WifiAP enabled
5. Not changing network IF there is Download/upload progress
6. Support For Both GSM and CDMA devices (need someone to test for CDMA devices)
7. every condition has an option :D
8. Restore all network configuration if battery saver mode disabled
9. Add Time Range to start battery saver mode
10. allow device without mobile data support, to using this feature
11. fix inconsistent battery low level
12. rewrite to better performance
13. modified the controller to support battery saver mode
14. add bluetooth, gps, wifi, and mobile data (if support) to be disabled while power saving activated
15. fix build QuickSettings because of LocationController change
16. rewrite to better performance
17. fix brightness value
18. rewrite (again) to allow add more saving mode
19. do delay changing only for POWER SAVING not NORMAL mode
20. add Cpu power saving mode
21. fix Cpu freq value
22. fix call saving state for non user configuration mode
23. fix NPE
24. add kill all task
25. add disable sync

Conflicts:
	res/values/custom_strings.xml

Change-Id: I19502c731b74e82a3873f19f51965ee5a88c1f04

Conflicts:
	AndroidManifest.xml
	res/values/custom_strings.xml
	src/com/android/settings/fuelgauge/PowerUsageSummary.java

[3/3] OmniGears: Battery Saver Mode

introduce battery saver mode
this based from what user want or what user do to saving the battery life
this include:
1. Automatically switch mobile network into what user choice and also di

src/com/android/settings/DataUsageSummary.java
@@ -142,6 +142,7 @@ import com.android.settings.net.UidDetailProvider;
 import com.android.settings.widget.ChartDataUsageView;
 import com.android.settings.widget.ChartDataUsageView.DataUsageChartListener;
 import com.android.settings.widget.PieChartView;
+import com.android.settings.liquid.batterysaver.BatterySaverHelper;
 import com.google.android.collect.Lists;
 
 import libcore.util.Objects;
@@ -215,6 +216,8 @@ public class DataUsageSummary extends Fragment {
     private LinearLayout mNetworkSwitches;
     private Switch mDataEnabled;
     private View mDataEnabledView;
+    private Switch mBatterySaverEnabled;
+    private View mBatterySaverEnabledView;
     private CheckBox mDisableAtLimit;
     private View mDisableAtLimitView;
 
@@ -356,6 +359,11 @@ public class DataUsageSummary extends Fragment {
             mDataEnabledView = inflatePreference(inflater, mNetworkSwitches, mDataEnabled);
             mDataEnabled.setOnCheckedChangeListener(mDataEnabledListener);
             mNetworkSwitches.addView(mDataEnabledView);
+
+            mBatterySaverEnabled = new Switch(inflater.getContext());
+            mBatterySaverEnabledView = inflatePreference(inflater, mNetworkSwitches, mBatterySaverEnabled);
+            mBatterySaverEnabled.setOnCheckedChangeListener(mBatterySaverEnabledListener);
+            mNetworkSwitches.addView(mBatterySaverEnabledView);
 
             mDisableAtLimit = new CheckBox(inflater.getContext());
             mDisableAtLimit.setClickable(false);
@@ -571,6 +579,7 @@ public class DataUsageSummary extends Fragment {
     @Override
     public void onDestroy() {
         mDataEnabledView = null;
+        mBatterySaverEnabledView = null;
         mDisableAtLimitView = null;
 
         mUidDetailProvider.clearCache();
@@ -708,23 +717,27 @@ public class DataUsageSummary extends Fragment {
         if (LOGD) Log.d(TAG, "updateBody() with currentTab=" + currentTab);
 
         mDataEnabledView.setVisibility(isOwner ? View.VISIBLE : View.GONE);
+        mBatterySaverEnabledView.setVisibility(isOwner ? View.VISIBLE : View.GONE);
 
         // TODO: remove mobile tabs when SIM isn't ready
         final TelephonyManager tele = TelephonyManager.from(context);
 
         if (TAB_MOBILE.equals(currentTab)) {
             setPreferenceTitle(mDataEnabledView, R.string.data_usage_enable_mobile);
+            setPreferenceTitle(mBatterySaverEnabledView, R.string.data_usage_enable_batterysaver);
             setPreferenceTitle(mDisableAtLimitView, R.string.data_usage_disable_mobile_limit);
             mTemplate = buildTemplateMobileAll(getActiveSubscriberId(context));
 
         } else if (TAB_3G.equals(currentTab)) {
             setPreferenceTitle(mDataEnabledView, R.string.data_usage_enable_3g);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             setPreferenceTitle(mDisableAtLimitView, R.string.data_usage_disable_3g_limit);
             // TODO: bind mDataEnabled to 3G radio state
             mTemplate = buildTemplateMobile3gLower(getActiveSubscriberId(context));
 
         } else if (TAB_4G.equals(currentTab)) {
             setPreferenceTitle(mDataEnabledView, R.string.data_usage_enable_4g);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             setPreferenceTitle(mDisableAtLimitView, R.string.data_usage_disable_4g_limit);
             // TODO: bind mDataEnabled to 4G radio state
             mTemplate = buildTemplateMobile4g(getActiveSubscriberId(context));
@@ -732,12 +745,14 @@ public class DataUsageSummary extends Fragment {
         } else if (TAB_WIFI.equals(currentTab)) {
             // wifi doesn't have any controls
             mDataEnabledView.setVisibility(View.GONE);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             mDisableAtLimitView.setVisibility(View.GONE);
             mTemplate = buildTemplateWifiWildcard();
 
         } else if (TAB_ETHERNET.equals(currentTab)) {
             // ethernet doesn't have any controls
             mDataEnabledView.setVisibility(View.GONE);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             mDisableAtLimitView.setVisibility(View.GONE);
             mTemplate = buildTemplateEthernet();
 
@@ -866,11 +881,24 @@ public class DataUsageSummary extends Fragment {
         }
     }
 
+    private boolean isBatterySaverEnabled() {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        return Settings.Global.getInt(resolver, Settings.Global.BATTERY_SAVER_OPTION, 0) != 0;
+    }
+
     private void setMobileDataEnabled(boolean enabled) {
         if (LOGD) Log.d(TAG, "setMobileDataEnabled()");
         mConnService.setMobileDataEnabled(enabled);
         mMobileDataEnabled = enabled;
         updatePolicy(true);
+    }
+
+    private void setBatterySaverModeEnabled(boolean enabled) {
+        if (LOGD) Log.d(TAG, "setBatterySaverModeEnabled()");
+        final Context context = getActivity();
+        BatterySaverHelper.setBatterySaverActive(context, enabled ? 1 : 0);
+        BatterySaverHelper.scheduleService(context);
+        updatePolicy(enabled);
     }
 
     private boolean isNetworkPolicyModifiable(NetworkPolicy policy) {
@@ -934,6 +962,7 @@ public class DataUsageSummary extends Fragment {
         if (TAB_MOBILE.equals(mCurrentTab)) {
             mBinding = true;
             mDataEnabled.setChecked(isMobileDataEnabled());
+            mBatterySaverEnabled.setChecked(isBatterySaverEnabled());
             mBinding = false;
         }
 
@@ -1045,6 +1074,20 @@ public class DataUsageSummary extends Fragment {
                     // calls setMobileDataEnabled() once user confirms.
                     ConfirmDataDisableFragment.show(DataUsageSummary.this);
                 }
+            }
+        }
+    };
+
+    private OnCheckedChangeListener mBatterySaverEnabledListener = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (mBinding) return;
+
+            final Context context = getActivity();
+            final boolean BatterySaverEnabled = isChecked;
+            final String currentTab = mCurrentTab;
+            if (TAB_MOBILE.equals(currentTab)) {
+                setBatterySaverModeEnabled(BatterySaverEnabled);
             }
         }
     };

src/com/android/settings/liquid/batterysaver/BatterySaverReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.liquid.batterysaver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BatterySaverReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        BatterySaverHelper.scheduleService(context);
+    }
+}

src/com/android/settings/liquid/batterysaver/BootReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.liquid.batterysaver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        BatterySaverHelper.scheduleService(context);
+    }
+}

wantowan2/wantowan2-android_packages_apps_Settings

Signed-off-by: scott <scott@ubuntu>
	res/values/bs_strings.xml
	res/values/config.xml
	res/xml/liquid_interface_settings.xml
	res/xml/power_usage_summary.xml
	src/com/android/settings/util/CMDProcessor.java
	src/com/android/settings/util/Helpers.java

src/com/android/settings/DataUsageSummary.java
@@ -142,6 +142,7 @@ import com.android.settings.net.UidDetailProvider;
 import com.android.settings.widget.ChartDataUsageView;
 import com.android.settings.widget.ChartDataUsageView.DataUsageChartListener;
 import com.android.settings.widget.PieChartView;
+import com.android.settings.beanstalk.batterysaver.BatterySaverHelper;
 import com.google.android.collect.Lists;
 
 import libcore.util.Objects;
@@ -215,6 +216,8 @@ public class DataUsageSummary extends Fragment {
     private LinearLayout mNetworkSwitches;
     private Switch mDataEnabled;
     private View mDataEnabledView;
+    private Switch mBatterySaverEnabled;
+    private View mBatterySaverEnabledView;
     private CheckBox mDisableAtLimit;
     private View mDisableAtLimitView;
 
@@ -356,6 +359,11 @@ public class DataUsageSummary extends Fragment {
             mDataEnabledView = inflatePreference(inflater, mNetworkSwitches, mDataEnabled);
             mDataEnabled.setOnCheckedChangeListener(mDataEnabledListener);
             mNetworkSwitches.addView(mDataEnabledView);
+
+            mBatterySaverEnabled = new Switch(inflater.getContext());
+            mBatterySaverEnabledView = inflatePreference(inflater, mNetworkSwitches, mBatterySaverEnabled);
+            mBatterySaverEnabled.setOnCheckedChangeListener(mBatterySaverEnabledListener);
+            mNetworkSwitches.addView(mBatterySaverEnabledView);
 
             mDisableAtLimit = new CheckBox(inflater.getContext());
             mDisableAtLimit.setClickable(false);
@@ -571,6 +579,7 @@ public class DataUsageSummary extends Fragment {
     @Override
     public void onDestroy() {
         mDataEnabledView = null;
+        mBatterySaverEnabledView = null;
         mDisableAtLimitView = null;
 
         mUidDetailProvider.clearCache();
@@ -708,23 +717,27 @@ public class DataUsageSummary extends Fragment {
         if (LOGD) Log.d(TAG, "updateBody() with currentTab=" + currentTab);
 
         mDataEnabledView.setVisibility(isOwner ? View.VISIBLE : View.GONE);
+        mBatterySaverEnabledView.setVisibility(isOwner ? View.VISIBLE : View.GONE);
 
         // TODO: remove mobile tabs when SIM isn't ready
         final TelephonyManager tele = TelephonyManager.from(context);
 
         if (TAB_MOBILE.equals(currentTab)) {
             setPreferenceTitle(mDataEnabledView, R.string.data_usage_enable_mobile);
+            setPreferenceTitle(mBatterySaverEnabledView, R.string.data_usage_enable_batterysaver);
             setPreferenceTitle(mDisableAtLimitView, R.string.data_usage_disable_mobile_limit);
             mTemplate = buildTemplateMobileAll(getActiveSubscriberId(context));
 
         } else if (TAB_3G.equals(currentTab)) {
             setPreferenceTitle(mDataEnabledView, R.string.data_usage_enable_3g);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             setPreferenceTitle(mDisableAtLimitView, R.string.data_usage_disable_3g_limit);
             // TODO: bind mDataEnabled to 3G radio state
             mTemplate = buildTemplateMobile3gLower(getActiveSubscriberId(context));
 
         } else if (TAB_4G.equals(currentTab)) {
             setPreferenceTitle(mDataEnabledView, R.string.data_usage_enable_4g);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             setPreferenceTitle(mDisableAtLimitView, R.string.data_usage_disable_4g_limit);
             // TODO: bind mDataEnabled to 4G radio state
             mTemplate = buildTemplateMobile4g(getActiveSubscriberId(context));
@@ -732,12 +745,14 @@ public class DataUsageSummary extends Fragment {
         } else if (TAB_WIFI.equals(currentTab)) {
             // wifi doesn't have any controls
             mDataEnabledView.setVisibility(View.GONE);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             mDisableAtLimitView.setVisibility(View.GONE);
             mTemplate = buildTemplateWifiWildcard();
 
         } else if (TAB_ETHERNET.equals(currentTab)) {
             // ethernet doesn't have any controls
             mDataEnabledView.setVisibility(View.GONE);
+            mBatterySaverEnabledView.setVisibility(View.GONE);
             mDisableAtLimitView.setVisibility(View.GONE);
             mTemplate = buildTemplateEthernet();
 
@@ -866,11 +881,24 @@ public class DataUsageSummary extends Fragment {
         }
     }
 
+    private boolean isBatterySaverEnabled() {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        return Settings.Global.getInt(resolver, Settings.Global.BATTERY_SAVER_OPTION, 0) != 0;
+    }
+
     private void setMobileDataEnabled(boolean enabled) {
         if (LOGD) Log.d(TAG, "setMobileDataEnabled()");
         mConnService.setMobileDataEnabled(enabled);
         mMobileDataEnabled = enabled;
         updatePolicy(true);
+    }
+
+    private void setBatterySaverModeEnabled(boolean enabled) {
+        if (LOGD) Log.d(TAG, "setBatterySaverModeEnabled()");
+        final Context context = getActivity();
+        BatterySaverHelper.setBatterySaverActive(context, enabled ? 1 : 0);
+        BatterySaverHelper.scheduleService(context);
+        updatePolicy(enabled);
     }
 
     private boolean isNetworkPolicyModifiable(NetworkPolicy policy) {
@@ -934,6 +962,7 @@ public class DataUsageSummary extends Fragment {
         if (TAB_MOBILE.equals(mCurrentTab)) {
             mBinding = true;
             mDataEnabled.setChecked(isMobileDataEnabled());
+            mBatterySaverEnabled.setChecked(isBatterySaverEnabled());
             mBinding = false;
         }
 
@@ -1045,6 +1074,20 @@ public class DataUsageSummary extends Fragment {
                     // calls setMobileDataEnabled() once user confirms.
                     ConfirmDataDisableFragment.show(DataUsageSummary.this);
                 }
+            }
+        }
+    };
+
+    private OnCheckedChangeListener mBatterySaverEnabledListener = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (mBinding) return;
+
+            final Context context = getActivity();
+            final boolean BatterySaverEnabled = isChecked;
+            final String currentTab = mCurrentTab;
+            if (TAB_MOBILE.equals(currentTab)) {
+                setBatterySaverModeEnabled(BatterySaverEnabled);
             }
         }
     };

src/com/android/settings/beanstalk/batterysaver/BatterySaverReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk.batterysaver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BatterySaverReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        BatterySaverHelper.scheduleService(context);
+    }
+}

src/com/android/settings/beanstalk/batterysaver/BootReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.beanstalk.batterysaver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        BatterySaverHelper.scheduleService(context);
+    }
+}