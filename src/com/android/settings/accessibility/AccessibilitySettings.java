TheKang/packages_apps_Settings

Slim Recents: Option to show topmost task [2/2]
Change-Id: I46cd6d1141ffb6893c94b10a7f0d9d9f94b92ec1

src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -107,6 +107,8 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             "screen_magnification_preference_screen";
     private static final String CUSTOM_RECENT_MODE =
             "custom_recent_mode";
+    private static final String CUSTOM_RECENT_SHOW_TOPMOST =
+            "custom_recent_show_topmost";
     private static final String SHAKE_SENSITIVITY =
             "shake_sensitivity";
     private static final String RECENT_PANEL_LEFTY_MODE =
@@ -216,6 +218,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
     private PreferenceScreen mDisplayMagnificationPreferenceScreen;
     private PreferenceScreen mGlobalGesturePreferenceScreen;
     private CheckBoxPreference mRecentsCustom;
+    private CheckBoxPreference mRecentsShowTopmost;
     private CheckBoxPreference mRecentPanelLeftyMode;
     private ListPreference mRecentPanelScale;
     private ListPreference mRecentPanelExpandedMode;
@@ -288,6 +291,11 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
                     Settings.System.CUSTOM_RECENT,
                     ((Boolean) newValue) ? true : false);
             Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mRecentsShowTopmost) {
+            Settings.System.putBoolean(getContentResolver(),
+                    Settings.System.CUSTOM_RECENT_SHOW_TOPMOST,
+                    ((Boolean) newValue) ? true : false);
             return true;
         } else if (preference == mRecentClearAll) {
             Settings.System.putInt(getActivity().getContentResolver(),
@@ -447,6 +455,12 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
         mRecentsCustom = (CheckBoxPreference) findPreference(CUSTOM_RECENT_MODE);
         mRecentsCustom.setChecked(enableRecentsCustom);
         mRecentsCustom.setOnPreferenceChangeListener(this);
+
+        boolean enableRecentsShowTopmost = Settings.System.getBoolean(getContentResolver(),
+                                      Settings.System.CUSTOM_RECENT_SHOW_TOPMOST, false);
+        mRecentsShowTopmost = (CheckBoxPreference) findPreference(CUSTOM_RECENT_SHOW_TOPMOST);
+        mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
+        mRecentsShowTopmost.setOnPreferenceChangeListener(this);
 
         mRecentPanelLeftyMode =
                 (CheckBoxPreference) findPreference(RECENT_PANEL_LEFTY_MODE);

wantowan2/wantowan2-android_packages_apps_Settings

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	res/values/slim_strings.xml

src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -104,6 +104,8 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             "captioning_preference_screen";
     private static final String DISPLAY_MAGNIFICATION_PREFERENCE_SCREEN =
             "screen_magnification_preference_screen";
+    private static final String RECENT_PANEL_SHOW_TOPMOST =
+            "recent_panel_show_topmost";
     private static final String SHAKE_SENSITIVITY =
             "shake_sensitivity";
     private static final String RECENT_PANEL_LEFTY_MODE =
@@ -208,6 +210,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
     private PreferenceScreen mCaptioningPreferenceScreen;
     private PreferenceScreen mDisplayMagnificationPreferenceScreen;
     private PreferenceScreen mGlobalGesturePreferenceScreen;
+    private CheckBoxPreference mRecentsShowTopmost;
     private CheckBoxPreference mRecentPanelLeftyMode;
     private ListPreference mRecentPanelScale;
     private ListPreference mRecentPanelExpandedMode;
@@ -272,6 +275,11 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                     Settings.System.RECENT_PANEL_GRAVITY,
                     ((Boolean) newValue) ? Gravity.LEFT : Gravity.RIGHT);
+            return true;
+        } else if (preference == mRecentsShowTopmost) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.RECENT_PANEL_SHOW_TOPMOST,
+                    ((Boolean) newValue) ? 1 : 0);
             return true;
         }
         return false;
@@ -415,6 +423,12 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             // nor long press power does not show global actions menu.
             mSystemsCategory.removePreference(mGlobalGesturePreferenceScreen);
         }
+
+        boolean enableRecentsShowTopmost = Settings.System.getInt(getContentResolver(),
+                                      Settings.System.RECENT_PANEL_SHOW_TOPMOST, 0) == 1;
+        mRecentsShowTopmost = (CheckBoxPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
+        mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
+        mRecentsShowTopmost.setOnPreferenceChangeListener(this);
 
         mRecentPanelLeftyMode =
                 (CheckBoxPreference) findPreference(RECENT_PANEL_LEFTY_MODE);