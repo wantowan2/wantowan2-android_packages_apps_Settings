a15e2b06fb90fcc60b40ed5a4dae875dbe00a222
Lars Greiss <kufikugel@googlemail.com>
2014-06-30 19:04:22 +0200

    Settings: Heads up meets SlimKat....SlimKat meets Google IO [4/4]
    For detailed explanation see here: https://gerrit.slimroms.net/#/c/9937/
    
    This patch adds the 4 possible user configurable settings.
    
    1. Enable always expanded heads up notifications.
    2. Set heads up snooze time
    3. Set timout timer
    4. Show updates of pending notifications
    
    5. Add in app info screen a checkbox to enable heads up for the app.
       As another possibility aside the longpress on the notification on
       notification drawer.
    
    Change-Id: I999df799cecb487c502e4a77626445813eeb197f
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	AndroidManifest.xml
    	res/layout/installed_app_details.xml
    	res/values/bs_arrays.xml
    	res/values/slim_strings.xml
    	src/com/android/settings/applications/InstalledAppDetails.java
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/bs_strings.xml

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -180,6 +180,7 @@ public class InstalledAppDetails extends Fragment
     private Button mBlacklistButton;
     private CompoundButton mNotificationSwitch;
     private CompoundButton mPrivacyGuardSwitch;
+    private CompoundButton mHeadsUpSwitch;
 
     private PackageMoveObserver mPackageMoveObserver;
     private AppOpsManager mAppOps;
@@ -557,6 +558,17 @@ public class InstalledAppDetails extends Fragment
         }
     }
 
+    private void initHeadsUpButton() {
+        boolean enabled = mPm.getHeadsUpSetting(mAppEntry.info.packageName);
+        mHeadsUpSwitch.setChecked(enabled);
+        if (isThisASystemPackage() || !mNotificationSwitch.isChecked()) {
+            mHeadsUpSwitch.setEnabled(false);
+        } else {
+            mHeadsUpSwitch.setEnabled(true);
+            mHeadsUpSwitch.setOnCheckedChangeListener(this);
+        }
+    }
+
     private void initPrivacyGuardButton() {
         if (mPrivacyGuardSwitch == null) {
             return;
@@ -673,6 +685,7 @@ public class InstalledAppDetails extends Fragment
         mPeekBlacklist = (CheckBox) mBlacklistDialogView.findViewById(R.id.peek_blacklist);
         mFloatingBlacklist = (CheckBox) mBlacklistDialogView.findViewById(R.id.floating_blacklist);
         mHoverBlacklist = (CheckBox) mBlacklistDialogView.findViewById(R.id.hover_blacklist);
+        mHeadsUpSwitch = (CompoundButton) view.findViewById(R.id.heads_up_switch);
 
         return view;
     }
@@ -1124,6 +1137,7 @@ public class InstalledAppDetails extends Fragment
         // only setup the privacy guard setting if we didn't get uninstalled
         if (!mMoveInProgress) {
             initPrivacyGuardButton();
+            initHeadsUpButton();
         }
 
         return true;
@@ -1452,6 +1466,10 @@ public class InstalledAppDetails extends Fragment
                         new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.cancel();
+                            // Give access to heads up check box.
+                            if (getOwner().mHeadsUpSwitch != null) {
+                                getOwner().mHeadsUpSwitch.setEnabled(true);
+                            }
                         }
                     })
                     .create();
@@ -1609,8 +1627,14 @@ public class InstalledAppDetails extends Fragment
         try {
             final boolean enable = mNotificationSwitch.isChecked();
             mNotificationManager.setNotificationsEnabledForPackage(mAppEntry.info.packageName, mAppEntry.info.uid, enabled);
+            if (mHeadsUpSwitch != null) {
+                mHeadsUpSwitch.setEnabled(enable);
+            }
         } catch (android.os.RemoteException ex) {
             mNotificationSwitch.setChecked(!enabled); // revert
+            if (mHeadsUpSwitch != null) {
+                mHeadsUpSwitch.setEnabled(!enabled);
+            }
         }
     }
 
@@ -1783,6 +1807,8 @@ public class InstalledAppDetails extends Fragment
             setFloatingModeState(isChecked);
         } else if (buttonView == mHoverBlacklist) {
             setHoverState(isChecked);
+        } else if (buttonView == mHeadsUpSwitch) {
+            mPm.setHeadsUpSetting(packageName, isChecked);
         }
     }
 }
